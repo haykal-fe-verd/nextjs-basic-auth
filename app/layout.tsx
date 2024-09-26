234fced3886c235e8e2f3f7da19ee73b3295da60
haykal-fe-verd <haykal@verd.net.id>
2024-09-26 16:18:54 +0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Poppins } from "next/font/google";
+
+import type { Metadata } from "next";
+
+import ThemeProvider from "@/components/theme-provider";
+
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Nextjs Auth Basic",
+  description: "By @kidz.eroll",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body className={`${poppins.className} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import ToggleMode from "@/components/toogle-mode";
+import React from "react";
+
+function Home() {
+  return (
+    <section className="container mx-auto">
+      <div>ini home</div>
+      <ToggleMode />
+    </section>
+  );
+}
+
+export default Home;
